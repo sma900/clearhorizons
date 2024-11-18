eeee5ee0ed500ff9b23df1b7d5ea508314c229ba
sma900 <102410385+sma900@users.noreply.github.com>
2024-11-18 13:41:14 +0530

    About Us commit final1

--- src/App.js
+++ src/App.js
@@ -10,8 +10,8 @@ function App() {
     <div className="App">
       <header className="App-header">
       {/* { <Slider></Slider> */
-      <Footer />
-      // <AboutUs/>
+      // <Footer />
+      <AboutUs/>
       
       }
 
